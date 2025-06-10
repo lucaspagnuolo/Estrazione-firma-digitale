855b80b1a175e4555dae3aa1659cf3e08718dc70
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-06-10 12:55:32 +0200

    Update Estrazione_firma_digitale.py

--- Estrazione_firma_digitale.py
+++ Estrazione_firma_digitale.py
@@ -404,38 +404,37 @@ if uploaded_files:
                 st.write(f"  – File estratto: **{payload_path.name}**")
                 st.write(f"    Firmato da: **{signer_name}**")
             with coly:
-                if firma_ok:
-                    st.success("Firma valida ✅")
-                else:
-                    st.error("Firma NON valida ⚠️")
 
-            # 6) Elimino la cartella temporanea
-            shutil.rmtree(temp_single, ignore_errors=True)
+                if firma_ok:
+                    st.success("Firma valida ✅")
+                else:
+                    st.error("Firma NON valida ⚠️")
 
-        else:
-            st.warning(f"Ignoro «{nome}»: estensione non supportata ({suff}).")
+            # 6) Elimino la cartella temporanea
+            shutil.rmtree(temp_single, ignore_errors=True)
 
-# --- Rimuovo cartelle duplicate con stesso contenuto ---
-    remove_duplicate_folders(root_temp)
+        else:
+            st.warning(f"Ignoro «{nome}»: estensione non supportata ({suff}).")
 
-# --- Creo lo ZIP di output con tutta la struttura “pulita” -------------
-    zip_out_path = root_temp / output_filename
-    with zipfile.ZipFile(zip_out_path, "w", zipfile.ZIP_DEFLATED) as zipf:
-        for root, _, files in os.walk(root_temp):
-            for file in files:
-                if file == output_filename:
-                    continue
-                file_path = Path(root) / file
-                rel_path = file_path.relative_to(root_temp)
-                zipf.write(file_path, rel_path)
+    # --- Rimuovo cartelle duplicate con stesso contenuto ---
+    remove_duplicate_folders(root_temp)
 
-# Pulsante per scaricare
-    with open(zip_out_path, "rb") as f:
-        st.download_button(
-            label="Scarica il file ZIP con tutte le estrazioni",
-            data=f,
-            file_name=output_filename,
-            mime="application/zip"
-        )
+    # --- Creo lo ZIP di output con tutta la struttura “pulita” -------------
+    zip_out_path = root_temp / output_filename
+    with zipfile.ZipFile(zip_out_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(root_temp):
+            for file in files:
+                if file == output_filename:
+                    continue
+                file_path = Path(root) / file
+                rel_path = file_path.relative_to(root_temp)
+                zipf.write(file_path, rel_path)
 
-
+    # Pulsante per scaricare
+    with open(zip_out_path, "rb") as f:
+        st.download_button(
+            label="Scarica il file ZIP con tutte le estrazioni",
+            data=f,
+            file_name=output_filename,
+            mime="application/zip"
+        )
